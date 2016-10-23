819b704d5d57bada510da7b7a06dbfa48e43d57b
Yardagan AL Hamed <gooryalhamed@hotmail.com>
2016-10-23 20:04:57 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,18 @@
 # Your code goes here!
+class Anagram
+  attr_accessor :word, :arr_letters
+  def initialize(word)
+    @word = word
+  @arr_letters= @word.split("")
+  end
+  #%w(enlists google inlets banana) == ["enlists", "google", "inlets", "banana"]
+  def match(arr)
+    anagram_arr = []
+    arr.each do |phrase|
+      if phrase.split("").sort == @arr_letters.sort then
+        anagram_arr << phrase
+      end
+    end
+    anagram_arr
+  end
+end
