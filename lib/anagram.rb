a7b1d32dcf71ba220277b110a02a77e7eea2a3be
fatma yousef <famyaltimimi@yahoo.com>
2016-11-24 16:51:18 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,18 @@
 # Your code goes here!
+class Anagram
+  attr_accessor :word
+  def initialize(word)
+    @word=word
+  end
+  def match(array)
+    result=[]
+    str=word.split("").sort
+    array.each do |w|
+    item=w.split("")
+      if item.sort==str
+        result<<item.join("")
+      end
+    end
+    return result
+  end
+end
